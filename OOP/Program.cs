aa62e44a12ed45cdddb2e0ee0105ad13edc2542f
XE <XE@XE1100>
2025-02-23 15:31:05 +0300

    Добавьте файлы проекта.

---
+++ OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using OOP;
+
+Cat murka = new Cat();
+Cat moska = new Cat();
+
+murka.age = 5;
+murka.name = "Murka";
+murka.color = "белая";
+murka.poroda = "сиамская";
+murka.height = 30;
+
+moska.age = 9;
+moska.name = "Moska";
+moska.color = "чёрная";
+moska.poroda = "бенгальская";
+moska.height = 35;
+
+
